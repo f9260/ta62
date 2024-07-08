8d50e70f6690c76f750ad8f949dc32336edd6c23
f9260 <wtfthisvideo@gmail.com>
2024-07-08 15:55:55 +0300

    first commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,14 +12,14 @@ const config: DocsThemeConfig = {
     text: (
       <span>
         {new Date().getFullYear()} ©{' '}
-        <a href="https://tr.ve3.plus/" target="_blank">
+        <a href="https://ve3.plus" target="_blank">
           VE3.Plus
         </a>
       </span>
     )
   },
   toc: {
-    title: "İçindekiler",
+    title: "On This Page",
     // Diğer TOC bileşeni özellikleri...
   },
   editLink: {
@@ -27,16 +27,13 @@ const config: DocsThemeConfig = {
     // Diğer TOC bileşeni özellikleri...
   },
   feedback: {
-    content: "Bir sorun mu var? Bizimle paylaş",
+    content: "Question? Let us know",
     useLink: () => "https://forum.ve3.plus/t/q-a"
     // Diğer TOC bileşeni özellikleri...
   },
-  search: {
-    placeholder: "Rehber'de Ara"
-  },
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – Rehber | VE3.Plus'
+      titleTemplate: '%s – Guide | VE3.Plus'
     }
   },
   gitTimestamp: null,
@@ -48,8 +45,8 @@ const config: DocsThemeConfig = {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="Rehber | VE3.Plus" />
-        <meta property="og:description" content="Rakipsiz video düzenleme otomasyonu VE3.Plus için oluşturulmuş kullanıcı rehberi." />
+        <meta property="og:title" content="Guide | VE3.Plus" />
+        <meta property="og:description" content="A user guide to VE3.Plus, the unrivaled video editing automation." />
       </Head>
     </>
   )
